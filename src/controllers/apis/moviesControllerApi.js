4195449a5c44c61f45875cc0a49bf890cae1bbb5
VERAPAO <pmnvera1984@gmail.com>
2024-06-02 19:16:09 -0300

    Se agregaron endpoints para listado de películas y detalle de película

--- src/controllers/apis/moviesControllerApi.js
+++ src/controllers/apis/moviesControllerApi.js
@@ -5,6 +5,70 @@ const { Op } = require("sequelize");
 
 
 const moviesControllerApi = {
+
+  'list': (req, res) => {
+    const countAllMovies = db.Movie.count({
+        attributes:{
+          exclude: ["created_at","updated_at"]
+        }          
+    })
+    const listMovies = db.Movie.findAll({
+        attributes:{
+          exclude: ["created_at","updated_at"]
+        }          
+    })
+
+    Promise.all([countAllMovies, listMovies])
+        .then(([count, movies]) => {
+            res.status(200).json({
+                meta: {
+                    status: 200,
+                    total: count,
+                    url: "api/movies"
+                },
+                data: {
+                    listMovies: movies
+                }
+            })
+        })
+        .catch((err) => {
+            res.status(500).json({
+              ok: false,
+              msg: err.message,
+            });
+          });
+  },
+
+  'detail': (req, res) => {
+    db.Movie.findByPk(req.params.id, {
+        attributes:{
+          exclude: ["created_at","updated_at"]
+        }          
+    })
+    .then(movie => {
+            if (!movie) {
+                return res.status(404).json({ error: "Película no encontrada" });
+            }
+            const movieData = {
+                id: movie.id,
+                title: movie.title,
+                rating: movie.rating,
+                awards: movie.awards,
+                release_date: movie.release_date,
+                lenght: movie.lenght,
+                genre_id: movie.genre_id
+            };
+
+            res.status(200).json({ok: true, data: movieData});
+    })
+    .catch((err) => {
+        res.status(500).json({
+          ok: false,
+          msg: err.message,
+        });
+      });
+  },
+
     'create': (req, res) => {
             
             db.Movie.create(
--- src/routes/api/moviesRoutesApi.js
+++ src/routes/api/moviesRoutesApi.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const moviesControllerApi = require('../../controllers/apis/moviesControllerApi');
 
+router.get('/api/movies', moviesControllerApi.list);
+router.get('/api/movies/detail/:id', moviesControllerApi.detail);
 router.post('/api/movies/create', moviesControllerApi.create);
 router.delete('/api/movies/delete/:id', moviesControllerApi.delete);
 
